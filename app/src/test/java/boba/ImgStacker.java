42763bc32e5ae259dcfe232043450bb62ff807ae
ioSquishy <squishylxix@gmail.com>
2023-02-12 00:24:07 -0800

    ability to set x and y coords for ImgStacker

--- app/src/test/java/boba/ImgStacker.java
+++ app/src/test/java/boba/ImgStacker.java
@@ -4,10 +4,12 @@ import java.awt.Image;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ImgStacker {
-    private final BufferedImage finalImage;
+    private final BufferedImage stackedImage;
     private final HashMap<String, Image> layers = new HashMap<>();
+    private final HashMap<String, int[]> transforms = new HashMap<>();
 
     /**
      * Constructs a {@link BufferedImage} object with ARGB type set automatically.
@@ -16,7 +18,7 @@ public class ImgStacker {
      * @param height Height of image.
      */
     public ImgStacker(int width, int height) {
-        finalImage = new BufferedImage(width, height, 2);
+        stackedImage = new BufferedImage(width, height, 2);
     }
 
     /**
@@ -30,6 +32,19 @@ public class ImgStacker {
     }
 
     /**
+     * Adds a layer to the final image with the specified image and offsets it from the top left corner with specified x and y values.
+     * 
+     * @param layerName What you want to call the layer.
+     * @param image The image you want on the layer.
+     * @param x X-coordinate from top-left corner to place image.
+     * @param y Y-coordinate from top-left corner to place image.
+     */
+    public void addLayer(String layerName, Image image, int x, int y) {
+        layers.put(layerName, image);
+        transforms.put(layerName, new int[] {x, y});
+    }
+
+    /**
      * Removes specified layer form the final image.
      * 
      * @param layerName Name of layer to remove.
@@ -37,6 +52,7 @@ public class ImgStacker {
      */
     public String removeLayer(String layerName) {
         layers.remove(layerName);
+        transforms.remove(layerName);
         return layerName;
     }
 
@@ -48,19 +64,41 @@ public class ImgStacker {
      */
     public void setLayer(String layerName, Image newImage) {
         layers.put(layerName, newImage);
-        
     }
 
     /**
-     * Returns the final image.
+     * Sets specified layer to the image provided and offsets it from the top-left corner by the specified x and y values.
+     * 
+     * @param layerName Layer to change.
+     * @param newImage Image to set layer to.
+     * @param x X-coordinate from top-left corner to place image.
+     * @param y Y-coordinate from top-left corner to place image.
+     */
+    public void setLayer(String layerName, Image newImage, int x, int y) {
+        layers.put(layerName, newImage);
+        transforms.put(layerName, new int[] {x, y});
+    }
+
+    /**
+     * Returns the stacked image.
      * 
      * @return Image
      */
-    public BufferedImage getFinalImage() {
-        Graphics graphics = finalImage.createGraphics();
-        for (Image layer : layers.values()) {
-            graphics.drawImage(layer, 0, 0, null);
+    public BufferedImage getStackedImage() {
+        Graphics graphics = stackedImage.createGraphics();
+        for (Map.Entry<String, Image> entry : layers.entrySet()) {
+            String layerName = entry.getKey();
+            Image image = entry.getValue();
+            
+            if (transforms.get(layerName) == null) {
+                graphics.drawImage(image, 0, 0, null);
+            } else {
+                graphics.drawImage(image, transforms.get(layerName)[0], transforms.get(layerName)[1], null);
+            }
         }
-        return finalImage;
+        /*for (Image layer : layers.values()) {
+            graphics.drawImage(layer, 0, 0, null);
+        }*/
+        return stackedImage;
     }
 }
--- app/src/test/java/boba/Test.java
+++ app/src/test/java/boba/Test.java
@@ -11,15 +11,15 @@ class Test {
         ImgStacker stack = new ImgStacker(100, 100);
 
         stack.addLayer("1", new ImageIcon("C:\\Users\\arthu\\Documents\\Boba\\app\\src\\test\\resources\\1.png").getImage());
-        stack.addLayer("2", new ImageIcon("C:\\Users\\arthu\\Documents\\Boba\\app\\src\\test\\resources\\2.png").getImage());
+        stack.addLayer("2", new ImageIcon("C:\\Users\\arthu\\Documents\\Boba\\app\\src\\test\\resources\\2.png").getImage(), 20, 20);
         stack.setLayer("1", new ImageIcon("C:\\Users\\arthu\\Documents\\Boba\\app\\src\\test\\resources\\3.png").getImage());
         stack.addLayer("4", new ImageIcon("C:\\Users\\arthu\\Documents\\Boba\\app\\src\\test\\resources\\4.png").getImage());
-        stack.removeLayer("1");
+        //stack.removeLayer("1");
 
 
         File outputfile = new File("output.png");
         try {
-            ImageIO.write(stack.getFinalImage(), "png", outputfile);
+            ImageIO.write(stack.getStackedImage(), "png", outputfile);
         } catch (IOException e) {
             e.printStackTrace();
         } // Write the Buffered Image into an output file
