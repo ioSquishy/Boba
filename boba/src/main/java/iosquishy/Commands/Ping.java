9de7712a6d955463968ac1c9b2a9627fab6ea6bd
ioSquishy <squishylxix@gmail.com>
2023-07-22 14:33:04 -0700

    maven inittt

---
+++ boba/src/main/java/iosquishy/Commands/Ping.java
@@ -0,0 +1,16 @@
+package iosquishy.Commands;
+
+import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
+
+public class Ping {
+    public static SlashCommandBuilder createCommand () {
+        return new SlashCommandBuilder()
+            .setName("ping")
+            .setDescription("Pong!");
+    }
+
+    public static void handleCommand (SlashCommandInteraction interaction) {
+        interaction.createImmediateResponder().setContent("Pong! `" + interaction.getApi().getLatestGatewayLatency().toMillis() + "ms`").respond();
+    }
+}
---
+++ boba/src/main/java/iosquishy/Utility/SettingCompiler.java
@@ -0,0 +1,29 @@
+package iosquishy.Utility;
+
+import java.awt.Image;
+import java.awt.Point;
+import java.util.HashMap;
+
+public class SettingCompiler {
+    public static enum Setting {
+        WOOD
+    }
+    private static HashMap<Setting, Image> settingImage = new HashMap<>(Setting.values().length);
+    private static HashMap<Setting, Point[]> bobaCoords = new HashMap<>(Setting.values().length);
+    public static void initSettingMaps() {
+        //WOOD:
+    }
+
+    public static Image compileSetting(Setting setting, Image[] bobas) {
+        //Create ImgStacker
+        ImgStacker compiledSetting = new ImgStacker(settingImage.get(setting).getWidth(null), settingImage.get(setting).getHeight(null));
+        //Add setting backdrop
+        compiledSetting.setLayer("backdrop", settingImage.get(setting));
+        //Add boba
+        for (int bobaImg = 0; bobaImg < bobas.length; bobaImg++) {
+            compiledSetting.setLayer(""+bobaImg, bobas[bobaImg], (int) bobaCoords.get(setting)[bobaImg].getX(), (int) bobaCoords.get(setting)[bobaImg].getY());
+        }
+        //Return
+        return compiledSetting.getStackedImage();
+    }
+}
